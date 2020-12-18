425c97ff26ea48cf6f6f65e435cc865005c22b9d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-12-18 11:05:46 +0800

    init

---
+++ src/test/java/cn/com/xuxiaowei/advice/AdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.advice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
